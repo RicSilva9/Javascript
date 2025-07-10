e5d668d47edfe8b57dc027d793b1441a105bac35
Ricardo da Silva Sousa <rssricardo82@gmail.com>
2025-07-10 06:10:44 -0300

    project: Implementing transfer

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -317,9 +317,10 @@ const displayMovements = function (movements) {
   });
 };
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}R$`;
+const calcDisplayBalance = function (acc) {
+  const balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  acc.balance = balance;
+  labelBalance.textContent = `${acc.balance}R$`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -353,37 +354,73 @@ const createUsernames = function (accs) {
       .join('');
   });
 };
+
 createUsernames(accounts);
+
+const updateUI = function(acc) {
+  // Display Movements
+  displayMovements(acc.movements);
+  
+  // Display Balance
+  calcDisplayBalance(acc);
+
+  // Display Summary
+  calcDisplaySummary(acc);
+}
 
 // Event handlers
 let currentAccount;
 
-btnLogin.addEventListener('click', function(e) {
+btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
 
-  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
   // console.log(currentAccount);
-  
-  if(currentAccount?.pin === Number(inputLoginPin.value)) {
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
     // Display UI and message
-    labelWelcome.textContent = `Bem-vindo(a) de volta, ${currentAccount.owner.split(' ')[0]}!`;
+    labelWelcome.textContent = `Bem-vindo(a) de volta, ${
+      currentAccount.owner.split(' ')[0]
+    }!`;
     containerApp.style.opacity = 100;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
-    // Display Movements
-    displayMovements(currentAccount.movements);
-
-    // Display Balance
-    calcDisplayBalance(currentAccount.movements);
-    
-    // Display Summary
-    calcDisplaySummary(currentAccount);
+    // Update UI
+    updateUI(currentAccount)
   }
-})
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount)
+
+    // Update UI
+    updateUI(currentAccount)
+  }
+
+});
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // LECTURES
