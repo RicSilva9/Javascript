77159baeedad8177112c4eb0a4e8ed091056a49b
Ricardo da Silva Sousa <rssricardo82@gmail.com>
2025-07-17 05:01:30 -0300

    projetct; add classificação dos movimentos da conta

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -415,11 +415,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
-  containerMovements.innerHTML = ''; // Clear existing movements
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = '';
 
-  // Display each movement
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -578,6 +579,13 @@ btnClose.addEventListener('click', function (e) {
 
   inputCloseUsername.value = inputClosePin.value = '';
 });
+
+let sorted = false;
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+})
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // LECTURES
@@ -743,14 +751,14 @@ console.log(overalBalance2);
 */
 
 // Stings
-const proprietários = ['Ricardo', 'Jonas', 'Adriano', 'Matheus'];
-console.log(proprietários);
-console.log(proprietários.sort()); // new method
-console.log(proprietários);
+// const proprietários = ['Ricardo', 'Jonas', 'Adriano', 'Matheus'];
+// console.log(proprietários);
+// console.log(proprietários.sort()); // new method
+// console.log(proprietários);
 
 // Numbers
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-console.log(movements);
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// console.log(movements);
 
 // return < 0, A, B (manter ordem)
 // return > 0, B, A (trocar ordem)
@@ -763,8 +771,8 @@ console.log(movements);
 //   if (b > a)
 //     return -1
 // })
-movements.sort((a, b) => a - b);
-console.log(movements);
+// movements.sort((a, b) => a - b);
+// console.log(movements);
 
 // DESCEDENTE
 // movements.sort((a, b) => {
@@ -773,5 +781,5 @@ console.log(movements);
 //   if (b > a)
 //     return 1
 // })
-movements.sort((a, b) => b - a);
-console.log(movements)
+// movements.sort((a, b) => b - a);
+// console.log(movements)
